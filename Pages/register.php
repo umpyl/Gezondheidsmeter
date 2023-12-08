cb7e4a21a27c469f85ff89e84b19bbdc921008f4
Rayanely <562891@edu.rocmn.nl>
2023-12-08 11:40:23 +0100

    ..

--- Pages/register.php
+++ Pages/register.php
@@ -10,6 +10,11 @@
 </head>
 
 <body>
+    <div class="head">
+        <svg class="pumping-heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M12 21.35l-1.45-1.32C5.4 14.25 2 11.28 2 7.5 2 4.42 4.42 2 7.5 2 9.43 2 11.11 3.11 12 4.74 12.89 3.11 14.57 2 16.5 2 19.58 2 22 4.42 22 7.5c0 3.78-3.4 6.75-8.55 12.54L12 21.35z" />
+        </svg>
+    </div>
     <div class="register_content">
         <form class="register_form" name="register_form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
             <div class="hoofd_txt">
@@ -17,25 +22,25 @@
             </div>
             <div class="form-field">
                 <label for="username">Naam:</label><span><br>
-                    <input type="text" id="username" name="_username" class="input_reg" autocomplete="off" />
+                    <input type="text" id="username" name="_username" class="input_reg" autocomplete="off" required />
                 </span>
             </div>
             <div class="form-field">
                 <label for="email">Email:</label><span><br>
-                    <input type="text" id="email" name="_email" class="input_reg" autocomplete="off" />
+                    <input type="text" id="email" name="_email" class="input_reg" autocomplete="off" required />
                 </span>
             </div>
             <div class="form-field">
                 <label for="credentials">Wachtwoord:</label><span><br>
-                    <input type="password" id="credentials" name="_passwd" class="input_reg" autocomplete="off" />
+                    <input type="password" id="credentials" name="_passwd" class="input_reg" autocomplete="off" required />
                 </span>
             </div>
             <div class="form-field">
                 <label for="credentials">Geslacht:</label><span><br>
-                    <input type="radio" id="man" name="gender" />
+                    <input type="radio" id="man" name="gender" required />
                     <label for="man">Man</label>
 
-                    <input type="radio" id="vrouw" name="gender" />
+                    <input type="radio" id="vrouw" name="gender" required />
                     <label for="vrouw">Vrouw</label>
                 </span>
             </div>
@@ -47,9 +52,7 @@
 
         </form>
 
-        <svg class="pumping-heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M12 21.35l-1.45-1.32C5.4 14.25 2 11.28 2 7.5 2 4.42 4.42 2 7.5 2 9.43 2 11.11 3.11 12 4.74 12.89 3.11 14.57 2 16.5 2 19.58 2 22 4.42 22 7.5c0 3.78-3.4 6.75-8.55 12.54L12 21.35z" />
-        </svg>
+
     </div>
 </body>
 
