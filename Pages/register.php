5d80809bc0ff040bca60414a6efceb1bfbaaef11
Rayanely <562891@edu.rocmn.nl>
2023-12-08 23:14:13 +0100

    -

--- Pages/register.php
+++ Pages/register.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require "../Particles/conn.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registreren</title>
-    <link rel="stylesheet" href="../../Assets/CSS/register.css">
+    <link rel="stylesheet" href="../Assets/CSS/register.css">
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 
