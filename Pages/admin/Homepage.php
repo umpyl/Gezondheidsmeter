f91213c62bfa3f8c29e67894d7643042488406c5
umpyl <566015@edu.rocmn.nl>
2023-12-13 10:28:29 +0100

    Homepage

--- Pages/admin/Homepage.php
+++ Pages/admin/Homepage.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+require "../../Particles/conn.php";
+$connectionClass = new Connection();
+$connection = $connectionClass->setConnection();
+
+$stmt = $connection->prepare("SELECT * FROM `gezond_questions`");
+$stmt->execute();
+$result = $stmt->get_result();
+
+if($result->num_rows >= 1){
+    $row = $result->fetch_assoc();
+    $question = $row["Questions"];
+}else {
+    $question = "No questions found"; 
+}
+
+//Vraag aanmaken
+//alle vragen zien / bewerken
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +27,12 @@
     <title>Homepage admin</title>
 </head>
 <body>
+<div id="header">
+    <button name="button_vraag_aanmaken">Vraag aanmaken</button>
+</div>
+<div class="styling">
+    <?php echo $question;?>
+</div>
     
 </body>
 </html>
