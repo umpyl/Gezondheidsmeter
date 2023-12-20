3f92ac3cb1016ff99fa7d39fe619aa14eb7fefd1
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-12-20 09:51:29 +0100

    redirect fix

--- Pages/login.php
+++ Pages/login.php
@@ -30,12 +30,12 @@ if (isset($_POST["submit"])) {
 
                 $_SESSION["naam"] = $naam;
                 $_SESSION["admin"] = true;
-                header("Location: ../admin/Homepage.php");
+                header("Location: admin/Homepage.php");
                 exit();
             } else {
                 $_SESSION["naam"] = $naam;
                 $_SESSION["admin"] = false;
-                header("Location: ../gebruiker/Homepage.php");
+                header("Location: gebruiker/Homepage.php");
                 exit();
             }
         } else {
