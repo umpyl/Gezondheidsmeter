94b23410fd44f1a65a3f1dbc7cac85f9486715d0
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2024-01-08 13:47:21 +0100

    removed useless charset

--- Particles/conn.php
+++ Particles/conn.php
@@ -4,7 +4,6 @@ class Connection
     public function setConnection() {
         include 'config.php';
         $con = mysqli_connect($dbhost,$user,$pass,$dbname);
-        mysqli_set_charset($con, "utf8mb4");
         // Check connection
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
