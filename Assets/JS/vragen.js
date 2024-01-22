d9ac3c784e92e26e0bd1b5eb925b2b961d27596d
umpyl <566015@edu.rocmn.nl>
2024-01-22 14:27:27 +0100

    vragen.php af

--- Assets/JS/vragen.js
+++ Assets/JS/vragen.js
@@ -1,5 +1,6 @@
 let currentQuestion = 1;
 let answer = 0;
+const questionAnswerArray = [];
 
 document.onload = loadQuestion();
 
@@ -11,35 +12,73 @@ function loadQuestion() {
 
 function checkanswer() {
 	let buttons = document.getElementsByName("yesno");
-
+	let isChecked = false;
+  
 	for (i = 0; i < buttons.length; i++) {
-		if (buttons[i].checked) {
-			answer = buttons[i].value;
-			buttons[i].checked = false;
-			break;
-		}
+	  if (buttons[i].checked) {
+		answer = buttons[i].value;
+		isChecked = true;
+		buttons[i].checked = false;
+		break;
+	  }
 	}
+  
+	if (!isChecked) {
+	  // Alert or notify the user that they need to select an answer
+	  alert("Please select an answer.");
+	}
+  
+	return isChecked;
+  }
+
+for (const questionObj of questionsArray) {
+	const newQuestionAnswerObj = {
+		Question: questionObj.Question,
+		Answer: ''
+	};
+	questionAnswerArray.push(newQuestionAnswerObj);
+}
+
+function CheckButton() {
+		if (questionAnswerArray[currentQuestion - 1].Answer == "1") {
+			document.getElementById("yes").checked = true;
+
+
+		} else if(questionAnswerArray[currentQuestion - 1].Answer == "0"){
+			document.getElementById("no").checked = true;
+		}
+	
 }
 
 function UpdateCurrentQuestion(action) {
 	if (action === "prev" && currentQuestion > 1) {
-		currentQuestion--;
+	  currentQuestion--;
+	  CheckButton();
 	}
-
+  
 	if (action === "next") {
-		let questionId = questionsArray[currentQuestion - 1].idQuestions;
-		console.log(questionId);
-		checkanswer();
-		sendAnswerToServer(questionId, userId, answer);
-
-		if (currentQuestion < QuestionCount) {
-			currentQuestion++;
-		} else {
-			console.log("Last question");
-		}
+	  let isRadioButtonChecked = checkanswer();
+  
+	  if (!isRadioButtonChecked) {
+		// Return early if no answer is selected
+		return;
+	  }
+  
+	  let questionId = questionsArray[currentQuestion - 1].idQuestions;
+	  sendAnswerToServer(questionId, userId, answer);
+  
+	  if (currentQuestion < QuestionCount) {
+		questionAnswerArray[currentQuestion - 1].Answer = answer;
+		currentQuestion++;
+		CheckButton();
+	  } else {
+		console.log("Last question");
+	  }
 	}
+  
 	loadQuestion();
-}
+  }
+
 
 function sendAnswerToServer(questionId, userId, answer) {
 	fetch("Save_answer.php", {
